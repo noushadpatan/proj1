c627a930dd57d1291e87b27a1b726aa081066dcc
riyazarnr <riyazshaik@Riyaz-Shaik.local>
2015-08-23 22:49:26 +0530

    Ns - basic SetUp

---
+++ proj22/proj22/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proj22
+//
+//  Created by riyaz shaik on 23/08/15.
+//  Copyright (c) 2015 riyazarnr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
